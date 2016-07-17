1672ac404fbac6f91406864860a60bb6ebfd9707
Andrew Hall <Andrew.Hall649@gmail.com>
2016-07-17 23:37:33 +1000

    Massive update since last time. Removed some old config and added new ones to reflect my current Arch VM setup.

--- .conky/TeejeeTech/alsavol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#awk -F"[][]" '/dB/ { gsub(/off/, "Muted"); gsub(/on/, "Unmuted"); print "Volume: " $2 " [" $6 "]" }' <(amixer sget Master) 
-
-vol=$(pulseaudio-ctl full-status)
-s=($vol)
-m=`echo "${s[1]}" | sed 's/no/Unmuted/g;s/yes/Muted/g'`
-echo Volume:  ${s[0]}\% \[${m}\]
--- .fluxbox/updatemenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mmaker --no-debian --no-legacy -f -t xterm fluxbox &&
-mv -f /home/andrew/.fluxbox/menu /home/andrew/.fluxbox/menu.backup &&
-awk '/\[exit\]/{print "\t\t[exec] (Power) {oblogout}"}1' /home/andrew/.fluxbox/menu.backup >/home/andrew/.fluxbox/menu &&
-mv -f /home/andrew/.fluxbox/menu /home/andrew/.fluxbox/menu.backup2 &&
-awk '/\[restart\]/{print "\t\t[exec] (Update Menu) {/home/andrew/.fluxbox/updatemenu.sh}"}1' /home/andrew/.fluxbox/menu.backup2 >/home/andrew/.fluxbox/menu &&
-rm /home/andrew/.fluxbox/menu.backup2
